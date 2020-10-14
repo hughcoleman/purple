6202e113f1265d7627123d90c93d08ba27b18b9f
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-10-14 13:17:18 -0700

    updated setup.py install script

--- setup.py
+++ setup.py
@@ -1,43 +1,53 @@
 #!/usr/bin/env python
-# Copyright (C) 2014 by Brian Neal.
-# This file is part of purple, the PURPLE (Cipher Machine 97) simulation.
-# purple is released under the MIT License (see LICENSE.txt).
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
-from os.path import join, dirname
+# setup.py
+# Copyright (c) 2014 Brian Neal
+# Copyright (c) 2020 Hugh Coleman
+#
+# This file is part of hughcoleman/purple, a historically accurate simulator of
+# the PURPLE (Type-B) Cipher Machine. It is released under the MIT License (see
+# LICENSE.)
 
+import setuptools
 import purple
 
-setup(
-    name='purple',
+from pathlib import Path
+
+with open(Path(__file__).parent / "README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="purple",
     version=purple.__version__,
-    author='Brian Neal',
-    author_email='bgneal@gmail.com',
-    url='https://bitbucket.org/bgneal/purple/',
-    license='MIT',
-    description='Simulation of the WW2 Japanese PURPLE cipher machine.',
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
-    packages=['purple', 'purple.tests'],
-    scripts=['scripts/purple'],
+    author="Hugh Coleman",
+    author_email="33557709+hughcoleman@users.noreply.github.com",
+    url="https://bitbucket.org/bgneal/purple/",
+    license="MIT",
+    description="A historically accurate simulator of the PURPLE (Type-B) Cipher Machine.",
+    long_description=long_description,
+    packages=["purple"],
+    scripts=["scripts/purple"],
     classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Other Audience',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Communications',
-        'Topic :: Security',
-        'Topic :: Security :: Cryptography',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Communications",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
     ],
+    python_requires=">=3.8"
 )
