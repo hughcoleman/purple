b502a88d960f90aba657f1dfa1e9de9c6db932e8
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-10-14 13:38:52 -0700

    tidied up the testing suite

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -1,107 +1,31 @@
-# Copyright (C) 2013 - 2014 by Brian Neal.
-# This file is part of purple, the PURPLE (Cipher Machine 97) simulation.
-# purple is released under the MIT License (see LICENSE.txt).
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import string
+# test_machine.py
+# Copyright (c) 2013 - 2014 Brian Neal
+# Copyright (c) 2020 Hugh Coleman
+# 
+# This file is part of hughcoleman/purple, a historically accurate simulator of
+# the PURPLE (Type-B) Cipher Machine. It is released under the MIT License (see
+# LICENSE.)
+
 import unittest
 
 from purple.machine import Purple97, Purple97Error
 from purple.switch import SteppingSwitchError
 
+import string
 
-# This is part 1 of the famous 14 part message. The ciphertext and plaintext are
-# laid on top of each other, every other line, with blank lines inserted for
-# clarity. Garbles are indicated by '-' characters.
-#
-part1 = """
-ZTXODNWKCCMAVNZXYWEETUQTCIMNVEUVIWBLUAXRRTLVA
-FOVTATAKIDASINIMUIMINOMOXIWOIRUBESIFYXXFCKZZR
+# This is the first part of the 14-part message which was delivered by the 
+# Japanese to the U.S. Government on December 7, 1941. Illegible characters are
+# indicated by dashes.
 
-RGNTPCNOIUPJLCIVRTPJKAUHVMUDTHKTXYZELQTVWGBUHFAWSH
-DXOOVBTNFYXFAEMEMORANDUMFIOFOVOOMOJIBAKARIFYXRAICC
+ciphertext = "ZTXODNWKCCMAVNZXYWEETUQTCIMNVEUVIWBLUAXRRTLVARGNTPCNOIUPJLCIVRTPJKAUHVMUDTHKTXYZELQTVWGBUHFAWSHULBFBHEXMYHFLOWD-KWHKKNXEBVPYHHGHEKXIOHQHUHWIKYJYHPPFEALNNAKIBOOZNFRLQCFLJTTSSDDOIOCVT-ZCKQTSHXTIJCNWXOKUFNQR-TAOIHWTATWVHOTGCGAKVANKZANMUINYOYJFSRDKKSEQBWKIOORJAUWKXQGUWPDUDZNDRMDHVHYPNIZXBGICXRMAWMFTIUDBXIENLONOQVQKYCOTVSHVNZZQPDLMXVNRUUNQFTCDFECZDFGMXEHHWYONHYNJDOVJUNCSUVKKEIWOLKRBUUSOZUIGNISMWUOSBOBLJXERZJEQYQMTFTXBJNCMJKVRKOTSOPBOYMKIRETINCPSQJAWVHUFKRMAMXNZUIFNOPUEMHGLOEJHZOOKHHEEDNIHXFXFXGPDZBSKAZABYEKYEPNIYSHVKFRFPVCJTPTOYCNEIQBFEXMERMIZLGDRXZORLZFSQYPZFATZCHUGRNHWDDTAIHYOOCOODUZYIWJROOJUMUIHRBEJFONAXGNCKAOARDIHCDZKIXPR--DIMUWOMHLTJSOUXPFKGEPWJOMTUVKMWRKTACUPIGAFEDFVRKXFXLFGURDETJIYOLKBHZKXOJDDOVRHMMUQBFOWRODMRMUWNAYKYPISDLHECKINLJORKWNWXADAJOLONOEVMUQDFIDSPEBBPWROFBOPAZJEUUSBHGIORCSUUQKIIEHPCTJRWSOGLETZLOUKKEOJOSMKJBWUCDDCPYUUWCSSKWWVLIUPKYXGKQOKAZTEZFHGVPJFEWEUBKLIZLWKKOBXLEPQPDATWUSUUPKYRHNWDZXXGTWDDNSHDCBCJXAOOEEPUBPWFRBQSFXSEZJJYAANMG-WLYMGWAQDGIVNOHKOUTIXYFOKNGGBFGANPWTUYLBEFFKUFLEXOIUUANVMMJEQUSFHFDOHQLAKWTBYYYLNTLYTSXCGKCEEWQRYAVGRKXIANPXNOFVXGKJFAVKLTHOCXCIVKOLXTJTUNCLQCICRUIIWQDDMOTPRVTJKKSKFHXFKMDIKIZWROGZJYMTMNOVMFJ-OKTEIVMYANOHNNYPDLEXCFRRNEBLMNYEBGNHCZZCFNWGGRHRIUUTTILKLODUYZKQOZMMNHASXHLPVTNGHQDAJIUGOOSZ-----ZRTGWFBLKI--------YBDABJ-----WYOEANV---OM"
+plaintext  = "FOVTATAKIDASINIMUIMINOMOXIWOIRUBESIFYXXFCKZZRDXOOVBTNFYXFAEMEMORANDUMFIOFOVOOMOJIBAKARIFYXRAICCYLFCBBCFCTHEGOVE-NMENTOFJAPANLFLPROMPTEDBYAGENUINEDESIRETOCOMETOANAMICABLEUNDERSTANDIN-WITHTHEGOVERNMENTOFTHE-NITEDSTATESINORDERTHATTHETWOCOUNTRIESBYTHEIRJOINTEFFORTSMAYSECURETHEPEACEOFTHEPACIFICAREAANDTHEREBYCONTRIBUTETOWARDTHEREALIZATIONOFWORLDPEACELFLHASCONTINUEDNEGOTIATIONSWITHTHEUTMOSTSINCERITYSINCEAPRILLASTWITHTHEGOVERNMENTOFTHEUNITEDSTATESREGARDINGTHEADJUSTMENTANDADVANCEMENTOFJAPANESEVVFAMERICANRELATIONSANDTHESTABILIZATIONOFTHEPACIFICAREACFCCCFTHEJAPANESEQOVERNMENXHASTHEHONORTOSTATEFRANKLYITSVIEWSCONCERNINGTHECLAIMSTHEAM--VCANGOVERNMENTHASUERSISTENTLYMAINTAINEDASWELLASTHEMEASURESTHEUNITEDSTATESANDGREATBRITAINHAVETAKENTOWARDJAPANDURINGTHKSEEIGHTMONTHSCYCCCFLFCDDCFCITISTHEIMMUTABLXPOLWCYOFTHEJAPANESEGOVERNMENTTOINSURETHESTABILITYOFEASTASIAANDTOPROMOTEWORLZPEACELFLANDTHEREBYTOEIABLEALLNATIONSTOFINDEACHITSPROPERPLACEINTHEWORLDCFCCCFEVERSINCETHE-HINAAFFAIRBROKEOUTOWINGTOTHEFAILUREONTHEPARTOFCHINATOCOMPREHENLJAPANVCFSTRUEYNTENTIONSLFLTWEJAPANESEGOVERNMENTHASSTRIVENFORTHERESTORATIONOFPEACEANDIMHASCONSISTENTLYEXERTEDITSBESTEFFORTSTOPREV-NTTHEEXTENTIONOFWARVVFLIKEVISTURBANCESCFCNSIASALSOTOTHATENDTNATINSEPTEMBERLASTYEARJAPANCONCL-----HETRIPAITI--------THGERM-----DYTALYC---OV"
 
-ULBFBHEXMYHFLOWD-KWHKKNXEBVPYHHGHEKXIOHQHUHWIKYJYH
-YLFCBBCFCTHEGOVE-NMENTOFJAPANLFLPROMPTEDBYAGENUINE
-
-PPFEALNNAKIBOOZNFRLQCFLJTTSSDDOIOCVT-ZCKQTSHXTIJCN
-DESIRETOCOMETOANAMICABLEUNDERSTANDIN-WITHTHEGOVERN
-
-WXOKUFNQR-TAOIHWTATWVHOTGCGAKVANKZANMUIN
-MENTOFTHE-NITEDSTATESINORDERTHATTHETWOCO
-
-YOYJFSRDKKSEQBWKIOORJAUWKXQGUWPDUDZNDRMDHVHYPNIZXB
-UNTRIESBYTHEIRJOINTEFFORTSMAYSECURETHEPEACEOFTHEPA
-
-GICXRMAWMFTIUDBXIENLONOQVQKYCOTVSHVNZZQPDLMXVNRUUN
-CIFICAREAANDTHEREBYCONTRIBUTETOWARDTHEREALIZATIONO
-
-QFTCDFECZDFGMXEHHWYONHYNJDOVJUNCSUVKKEIWOLKRBUUSOZ
-FWORLDPEACELFLHASCONTINUEDNEGOTIATIONSWITHTHEUTMOS
-
-UIGNISMWUOSBOBLJXERZJEQYQMTFTXBJNCMJKVRKOTSOPBOYMK
-TSINCERITYSINCEAPRILLASTWITHTHEGOVERNMENTOFTHEUNIT
-
-IRETINCPSQJAWVHUFKRMAMXNZUIFNOPUEMHGLOEJHZOOKHHEED
-EDSTATESREGARDINGTHEADJUSTMENTANDADVANCEMENTOFJAPA
-
-NIHXFXFXGPDZBSKAZABYEKYEPNIYSHVKFRFPVCJTPTOYCNEIQB
-NESEVVFAMERICANRELATIONSANDTHESTABILIZATIONOFTHEPA
-
-FEXMERMIZLGDRXZORLZFSQYPZFATZCHUGRNHWDDTAIHYOOCOOD
-CIFICAREACFCCCFTHEJAPANESEQOVERNMENXHASTHEHONORTOS
-
-UZYIWJROOJUMUIHRBEJFONAXGNCKAOARDIHCDZKIXPR--DIMUW
-TATEFRANKLYITSVIEWSCONCERNINGTHECLAIMSTHEAM--VCANG
-
-OMHLTJSOUXPFKGEPWJOMTUVKMWRKTACUPIGAFEDFVRKXFXLFGU
-OVERNMENTHASUERSISTENTLYMAINTAINEDASWELLASTHEMEASU
-
-RDETJIYOLKBHZKXOJDDOVRHMMUQBFOWRODMRMUWNAYKYPISDLH
-RESTHEUNITEDSTATESANDGREATBRITAINHAVETAKENTOWARDJA
-
-ECKINLJORKWNWXADAJOLONOEVMUQDFIDSPEBBPWROFBOPAZJEU
-PANDURINGTHKSEEIGHTMONTHSCYCCCFLFCDDCFCITISTHEIMMU
-
-USBHGIORCSUUQKIIEHPCTJRWSOGLETZLOUKKEOJOSMKJBWUCDD
-TABLXPOLWCYOFTHEJAPANESEGOVERNMENTTOINSURETHESTABI
-
-CPYUUWCSSKWWVLIUPKYXGKQOKAZTEZFHGVPJFEWEUBKLIZLWKK
-LITYOFEASTASIAANDTOPROMOTEWORLZPEACELFLANDTHEREBYT
-
-OBXLEPQPDATWUSUUPKYRHNWDZXXGTWDDNSHDCBCJXAOOEEPUBP
-OEIABLEALLNATIONSTOFINDEACHITSPROPERPLACEINTHEWORL
-
-WFRBQSFXSEZJJYAANMG-WLYMGWAQDGIVNOHKOUTIXYFOKNGGBF
-DCFCCCFEVERSINCETHE-HINAAFFAIRBROKEOUTOWINGTOTHEFA
-
-GANPWTUYLBEFFKUFLEXOIUUANVMMJEQUSFHFDOHQLAKWTBYYYL
-ILUREONTHEPARTOFCHINATOCOMPREHENLJAPANVCFSTRUEYNTE
-
-NTLYTSXCGKCEEWQRYAVGRKXIANPXNOFVXGKJFAVKLTHOCXCIVK
-NTIONSLFLTWEJAPANESEGOVERNMENTHASSTRIVENFORTHEREST
-
-OLXTJTUNCLQCICRUIIWQDDMOTPRVTJKKSKFHXFKMDIKIZWROGZ
-ORATIONOFPEACEANDIMHASCONSISTENTLYEXERTEDITSBESTEF
-
-JYMTMNOVMFJ-OKTEIVMYANOHNNYPDLEXCFRRNEBLMNYEBGNHCZ
-FORTSTOPREV-NTTHEEXTENTIONOFWARVVFLIKEVISTURBANCES
-
-ZCFNWGGRHRIUUTTILKLODUYZKQOZMMNHASXHLPVTNGHQDAJIUG
-CFCNSIASALSOTOTHATENDTNATINSEPTEMBERLASTYEARJAPANC
-
-OOSZ-----ZRTGWFBLKI--------YBDABJ-----WYOEANV---OM
-ONCL-----HETRIPAITI--------THGERM-----DYTALYC---OV
-"""
-
-pt1_lines = part1.split()
-PT1_CT = ''.join(pt1_lines[0::2])
-PT1_PT = ''.join(pt1_lines[1::2])
-
-
-class Purple97TestCase(unittest.TestCase):
+class TestPurple97(unittest.TestCase):
 
     def test_construction(self):
-
         Purple97()
         Purple97([0, 1, 2, 3])
         Purple97([0, 1, 2, 3], 1)
@@ -112,14 +36,12 @@ class Purple97TestCase(unittest.TestCase):
         Purple97(fast_switch=3, middle_switch=1)
 
     def test_construct_bad_positions(self):
-
         self.assertRaises(Purple97Error, Purple97, [])
         self.assertRaises(Purple97Error, Purple97, [1])
         self.assertRaises(Purple97Error, Purple97, [1, 1, 1, 1, 1])
         self.assertRaises(SteppingSwitchError, Purple97, [1, 1, 1, 100])
 
     def test_construct_bad_switches(self):
-
         self.assertRaises(Purple97Error, Purple97, fast_switch=0)
         self.assertRaises(Purple97Error, Purple97, fast_switch=4)
         self.assertRaises(Purple97Error, Purple97, fast_switch=-1)
@@ -129,109 +51,81 @@ class Purple97TestCase(unittest.TestCase):
         self.assertRaises(Purple97Error, Purple97, middle_switch=-1)
 
         self.assertRaises(Purple97Error, Purple97, fast_switch=2)
-        self.assertRaises(Purple97Error, Purple97, fast_switch=1, middle_switch=1)
-        self.assertRaises(Purple97Error, Purple97, fast_switch=2, middle_switch=2)
-        self.assertRaises(Purple97Error, Purple97, fast_switch=3, middle_switch=3)
+        self.assertRaises(Purple97Error, Purple97, fast_switch=1, 
+            middle_switch=1)
+        self.assertRaises(Purple97Error, Purple97, fast_switch=2, 
+            middle_switch=2)
+        self.assertRaises(Purple97Error, Purple97, fast_switch=3, 
+            middle_switch=3)
 
-        self.assertRaises(Purple97Error, Purple97, fast_switch=0, middle_switch=1)
-        self.assertRaises(Purple97Error, Purple97, fast_switch=0, middle_switch=0)
-        self.assertRaises(Purple97Error, Purple97, fast_switch=0, middle_switch=4)
+        self.assertRaises(Purple97Error, Purple97, fast_switch=0, 
+            middle_switch=1)
+        self.assertRaises(Purple97Error, Purple97, fast_switch=0, 
+            middle_switch=0)
+        self.assertRaises(Purple97Error, Purple97, fast_switch=0, 
+            middle_switch=4)
 
     def test_construct_bad_alphabet(self):
+        alphabets = [
+            "",
+            "1",
+            "!" * 26,
+            "DEFGHIJKLMNOPQRSTUVWXYZ",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZEFGHIJ",
+            "ABCDEFGHIJKLMABCDEFGHIJKLM",
+            "M" * 26
+        ]
 
-        alpha = ''
-        self.assertRaises(Purple97Error, Purple97, alphabet=alpha)
-        alpha = '1'
-        self.assertRaises(Purple97Error, Purple97, alphabet=alpha)
-        alpha = '!' * 26
-        self.assertRaises(Purple97Error, Purple97, alphabet=alpha)
-        alpha = string.ascii_uppercase[3:]
-        self.assertRaises(Purple97Error, Purple97, alphabet=alpha)
-        alpha = string.ascii_uppercase + string.ascii_uppercase[4:10]
-        self.assertRaises(Purple97Error, Purple97, alphabet=alpha)
-        alpha = string.ascii_uppercase[:13] + string.ascii_uppercase[:13]
-        self.assertRaises(Purple97Error, Purple97, alphabet=alpha)
-        alpha = 'M' * 26
-        self.assertRaises(Purple97Error, Purple97, alphabet=alpha)
+        for alphabet in alphabets:        
+            self.assertRaises(Purple97Error, Purple97, alphabet=alphabet)
 
     def test_from_key_sheet(self):
-
-        Purple97.from_key_sheet('9-1,2,3-23')
-        Purple97.from_key_sheet('1-1,1,1-13')
-        Purple97.from_key_sheet('25-25,25,25-31')
-        Purple97.from_key_sheet('5-20,7,18-21', alphabet=string.ascii_uppercase)
+        Purple97.from_key_sheet("9-1,2,3-23")
+        Purple97.from_key_sheet("1-1,1,1-13")
+        Purple97.from_key_sheet("25-25,25,25-31")
+        Purple97.from_key_sheet("5-20,7,18-21", alphabet=string.ascii_uppercase)
 
     def test_bad_from_key_sheet(self):
+        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, "0-1,2,3-13")
+        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, "26-1,2,3-13")
+        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, "1-1,0,3-13")
+        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, "1-1,2,26-13")
+        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, "1-1,2,26-03")
+        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, "1-1,2,26-00")
+        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, "1-1,2,26-14")
 
-        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, '0-1,2,3-13')
-        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, '26-1,2,3-13')
-        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, '1-1,0,3-13')
-        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, '1-1,2,26-13')
-        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, '1-1,2,26-03')
-        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, '1-1,2,26-00')
-        self.assertRaises(SteppingSwitchError, Purple97.from_key_sheet, '1-1,2,26-14')
-
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, 'bad string')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, '1-2-1,2,26-14')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, 'a-9,2,20-13')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, '1-a,2,20-13')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, '1-9,a,20-13')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, '1-9,2,a-13')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, '1-9,2,20-a3')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, '1-9,2,20-1a')
-        self.assertRaises(Purple97Error, Purple97.from_key_sheet, '1-9,2,20-123')
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "bad string")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "1-2-1,2,26-14")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "a-9,2,20-13")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "1-a,2,20-13")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "1-9,a,20-13")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "1-9,2,a-13")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "1-9,2,20-a3")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "1-9,2,20-1a")
+        self.assertRaises(Purple97Error, Purple97.from_key_sheet, "1-9,2,20-123")
 
     def test_decrypt_part_1_message(self):
-
-        ciphertext = PT1_CT
-        plaintext = PT1_PT
-
         self.assertEqual(len(ciphertext), len(plaintext))
 
-        # Decrypt
-        purple = Purple97.from_key_sheet(
-                switches='9-1,24,6-23',
-                alphabet='NOKTYUXEQLHBRMPDICJASVWGZF')
+        machine = Purple97.from_key_sheet(
+            switches="9-1,24,6-23",
+            alphabet="NOKTYUXEQLHBRMPDICJASVWGZF")
 
-        actual = purple.decrypt(ciphertext)
-
-        mismatches = []
-        for n, (a, b) in enumerate(zip(plaintext, actual)):
-            if a != b:
-                mismatches.append((n, a, b))
-
-        msg = None
-        if mismatches:
-            msg = "There are {} mismatches: {}".format(len(mismatches),
-                    mismatches)
-
-        self.assertTrue(len(mismatches) == 0, msg)
+        self.assertEqual(plaintext, machine.decrypt(ciphertext))
 
     def test_encrypt_part_1_message(self):
-
-        ciphertext = PT1_CT
-        plaintext = PT1_PT
-
-        # Use 'X' in place of the garbles
-        input_text = plaintext.replace('-', 'X')
-
         self.assertEqual(len(ciphertext), len(plaintext))
 
-        # Encrypt
-        purple = Purple97.from_key_sheet(
-                switches='9-1,24,6-23',
-                alphabet='NOKTYUXEQLHBRMPDICJASVWGZF')
+        machine = Purple97.from_key_sheet(
+            switches="9-1,24,6-23",
+            alphabet="NOKTYUXEQLHBRMPDICJASVWGZF")
 
-        actual = purple.encrypt(input_text)
+        # substitute illegible characters with "X"s.
+        plaintext_ = plaintext.replace("-", "X")
 
-        mismatches = []
-        for n, (a, b) in enumerate(zip(ciphertext, actual)):
-            if a != b and a != '-':
-                mismatches.append((n, a, b))
+        mismatches = 0
+        for ct, ept in zip(ciphertext, machine.encrypt(plaintext_)):
+            if ct != ept and ct != "-":
+                mismatches = mismatches + 1
 
-        msg = None
-        if mismatches:
-            msg = "There are {} mismatches: {}".format(len(mismatches),
-                    mismatches)
-
-        self.assertTrue(len(mismatches) == 0, msg)
+        self.assertTrue(mismatches <= 0)
--- tests/test_switch.py
+++ tests/test_switch.py
@@ -1,87 +1,97 @@
-# Copyright (C) 2013 by Brian Neal.
-# This file is part of purple, the PURPLE (Cipher Machine 97) simulation.
-# purple is released under the MIT License (see LICENSE.txt).
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# test_switch.py
+# Copyright (c) 2013 Brian Neal
+# Copyright (c) 2020 Hugh Coleman
+# 
+# This file is part of hughcoleman/purple, a historically accurate simulator of
+# the PURPLE (Type-B) Cipher Machine. It is released under the MIT License (see
+# LICENSE.)
 
 import unittest
 
 from purple.switch import SteppingSwitch, SteppingSwitchError, create_switch
 import purple.switch
 
-
-class SwitchTestCase(unittest.TestCase):
+class TestSwitch(unittest.TestCase):
 
     def test_construction(self):
-
         bad_wiring = [
             [1, 2, 0],
             [2, 1],
             [0, 1, 2]
         ]
+
         good_wiring = [
             [1, 2, 0],
             [2, 1, 0],
             [0, 1, 2]
         ]
+
         self.assertRaises(SteppingSwitchError, SteppingSwitch, bad_wiring)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, bad_wiring,
-                init_pos=0)
+            init_pos=0)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, good_wiring,
-                init_pos=-1)
+            init_pos=-1)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, good_wiring,
-                init_pos=3)
+            init_pos=3)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, bad_wiring,
-                init_pos=3)
+            init_pos=3)
 
     def test_wiring_table_mismatch_in_dimensions(self):
-
         wiring_1 = [
             [1, 2, 0],
             [2, 1, 0],
             [0, 1, 2],
         ]
+
         wiring_2 = [
             [1, 2, 3, 0],
             [1, 2, 3, 0],
             [1, 2, 3, 0],
         ]
+
         wiring_3 = [
             [1, 2, 3, 0],
             [1, 2, 3, 0],
             [1, 2, 3, 0],
             [1, 2, 3, 0],
         ]
+
         bad_wiring = [
             [1, 2, 0],
             [2, 1],
             [0, 1, 2]
         ]
+
         self.assertRaises(SteppingSwitchError, SteppingSwitch, wiring_1,
-                wiring_2)
+            wiring_2)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, wiring_2,
-                wiring_1)
+            wiring_1)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, wiring_1,
-                bad_wiring)
+            bad_wiring)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, wiring_2,
-                wiring_3)
+            wiring_3)
         self.assertRaises(SteppingSwitchError, SteppingSwitch, wiring_3,
-                wiring_2)
+            wiring_2)
 
     def test_bad_set_pos(self):
-
         switch = create_switch(purple.switch.SIXES)
+
         self.assertRaises(SteppingSwitchError, switch.set_pos, -1)
         self.assertRaises(SteppingSwitchError, switch.set_pos, 25)
         self.assertRaises(SteppingSwitchError, switch.set_pos, 26)
 
     def test_set_pos(self):
-
         switch = create_switch(purple.switch.SIXES)
+
         for pos in range(25):
             switch.set_pos(pos)
 
     def test_step(self):
-
         switch = create_switch(purple.switch.SIXES)
+
         for n in range(25 * 2 + 1):
             pos = n % 25
             self.assertEqual(switch.pos, pos)
@@ -89,8 +99,8 @@ class SwitchTestCase(unittest.TestCase):
             self.assertEqual((pos + 1) % 25, new_pos)
 
     def test_decrypt(self):
-
         switch = create_switch(purple.switch.SIXES)
+
         switch.set_pos(0)
         self.assertEqual(switch.decrypt(0), 1)
         self.assertEqual(switch.decrypt(1), 0)
@@ -116,8 +126,8 @@ class SwitchTestCase(unittest.TestCase):
         self.assertEqual(switch.decrypt(5), 0)
 
     def test_encrypt(self):
-
         switch = create_switch(purple.switch.SIXES)
+
         switch.set_pos(0)
         self.assertEqual(switch.encrypt(0), 1)
         self.assertEqual(switch.encrypt(1), 0)
@@ -143,7 +153,6 @@ class SwitchTestCase(unittest.TestCase):
         self.assertEqual(switch.encrypt(5), 4)
 
     def test_bad_create_switch(self):
-
         self.assertRaises(ValueError, create_switch, -1)
         self.assertRaises(ValueError, create_switch, 4)
         self.assertRaises(ValueError, create_switch, 99)
